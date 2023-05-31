f39d7dc100c395856084dccbc80a7206310f815e
Mei Inoue <110728083+inomeu@users.noreply.github.com>
2023-05-31 15:00:22 +0900

    [Update] Update map range

--- servo_tof1.ino
+++ servo_tof1.ino
@@ -2,16 +2,10 @@
 #include <Wire.h>
 #include <VL53L0X.h>
 
-VL53L0X sensor; //VL53L0X　"sensor"として操作することを宣言している。
+VL53L0X sensor; //VL53L0X　"sensor"とする
 
 //servo
-#define SERVO_PIN 26
-//PortB
-//#define SERVO_CH 0
-//#define PMW_HZ 50
-//#define PMW_BIT 16
-//#define MAX 7800
-//#define MIN 1700
+#define SERVO_PIN 26 //PortB
 #define SERVO_MIN_ANGLE 0
 #define SERVO_MAX_ANGLE 90
 
@@ -30,12 +24,7 @@ void setup() {
   //servo
   ledcSetup(0, 50, 16);
   ledcAttachPin(SERVO_PIN, 0);
-//  M5.Lcd.setBrightness(200);
-//  M5.Lcd.setTextSize(2);
-//  M5.Lcd.setTextFont(7);
-//  M5.Lcd.setTextColor(GREEN);
 }
-
 
 void loop() {
   int distance=0;
@@ -46,6 +35,6 @@ void loop() {
   }
   Serial.println();
 
-  int angle = map(distance, 5, 50, SERVO_MIN_ANGLE, SERVO_MAX_ANGLE);
+  int angle = map(distance, 10, 100, SERVO_MIN_ANGLE, SERVO_MAX_ANGLE);
     ledcWrite(0, angle);
 }
